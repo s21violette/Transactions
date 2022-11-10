1a53552c18bfd311a3759cf12edbd0c4aab18ef4
abceff <kunakov.rafael@gmail.com>
2022-11-10 18:26:21 +0300

    set function added

--- src/AbstractClass.h
+++ src/AbstractClass.h
@@ -1,21 +1,25 @@
 #ifndef TRANSACTIONS_ABSTRACTCLASS_H
 #define TRANSACTIONS_ABSTRACTCLASS_H
 
+#include <string>
+
+namespace s21 {
 class AbstractClass {
 public:
     virtual ~AbstractClass() = default;
-    virtual void Set() = 0;
-    virtual void Get() = 0;
-    virtual void Exists() = 0;
-    virtual void Del() = 0;
-    virtual void Update() = 0;
-    virtual void Keys() = 0;
-    virtual void Rename() = 0;
-    virtual void TTL() = 0;
-    virtual void Find() = 0;
-    virtual void Snowfall() = 0;
-    virtual void Upload() = 0;
-    virtual void Export() = 0;
+    virtual void Set(std::string str) = 0;
+    virtual void Get(std::string str) = 0;
+    virtual void Exists(std::string str) = 0;
+    virtual void Del(std::string str) = 0;
+    virtual void Update(std::string str) = 0;
+    virtual void Keys(std::string str) = 0;
+    virtual void Rename(std::string str) = 0;
+    virtual void TTL(std::string str) = 0;
+    virtual void Find(std::string str) = 0;
+    virtual void Snowfall(std::string str) = 0;
+    virtual void Upload(std::string str) = 0;
+    virtual void Export(std::string str) = 0;
 };
+}  // namespace s21
 
 #endif  // TRANSACTIONS_ABSTRACTCLASS_H
--- src/HashTable/HashTable.cpp
+++ src/HashTable/HashTable.cpp
@@ -1,9 +1,100 @@
 #include "HashTable.h"
 
-void Start() {
-
+namespace s21 {
+HashTable::HashTable() {
+    values_vector.resize(3);
+    for (Values* values : values_vector) {
+        values = nullptr;
+    }
 }
 
-void HashTable::Set() {
+HashTable::~HashTable() {
+    for (Values* values : values_vector) {
+        if (values) delete values;
+    }
+}
 
+void HashTable::Start() {
+    std::string function, operands;
+    std::cin >> function;
+    getline(std::cin, operands);
+    std::cout << "function == " << function << std::endl;
+    std::cout << "operands == " << operands << std::endl;
+    if (function == "SET") {
+        Set(operands);
+    } else if (function == "GET") {
+        std::cout << "GET function" << std::endl;
+    } else if (function == "EXISTS") {
+        std::cout << "EXISTS function" << std::endl;
+    } else if (function == "DEL") {
+        std::cout << "DEL function" << std::endl;
+    } else if (function == "UPDATE") {
+        std::cout << "UPDATE function" << std::endl;
+    } else if (function == "KEYS") {
+        std::cout << "KEYS function" << std::endl;
+    } else if (function == "RENAME") {
+        std::cout << "RENAME function" << std::endl;
+    } else if (function == "TTL") {
+        std::cout << "TTL function" << std::endl;
+    } else if (function == "FIND") {
+        std::cout << "FIND function" << std::endl;
+    } else if (function == "SHOWALL") {
+        std::cout << "SHOWALL function" << std::endl;
+    } else if (function == "UPLOAD") {
+        std::cout << "UPLOAD function" << std::endl;
+    } else if (function == "EXPORT") {
+        std::cout << "EXPORT function" << std::endl;
+    } else {
+        std::cout << "Invalid function" << std::endl;
+    }
+}
+
+void HashTable::Set(std::string operands) {
+    std::istringstream iss(operands);
+    std::string key;
+    Values values;
+    if (!(iss >> key) || !(iss >> values.last_name_) || !(iss >> values.first_name_) ||
+        !(iss >> values.year_of_birth_) || !(iss >> values.city_) || !(iss >> values.number_of_coins_)) {
+        std::cout << "Invalid operands" << std::endl;
+    } else {
+        values.key_ = key;
+        if (number_of_indexes_filled_in == values_vector.size()) {
+            values_vector.resize(number_of_indexes_filled_in + buffer_size_);
+        }
+        int key_int = StringToKeyInt(key), i = 0;
+        std::cout << "key_int == " << key_int << std::endl;
+        while (true) {
+            int index = (key_int + i * HashFunction2(key_int)) % values_vector.size();
+            std::cout << "index == " << index << std::endl;
+            if (!values_vector[index]) {
+                values_vector[index] = new Values(values);
+                ++number_of_indexes_filled_in;
+                break;
+            } else if (values_vector[index]->key_ == key) {
+                std::cout << "The key already exists" << std::endl;
+                break;
+            }
+            ++i;
+        }
+    }
+}
+
+int HashTable::StringToKeyInt(std::string str) {
+    int key_int = 0;
+    for (char c : str) {
+        key_int += c;
+    }
+    return key_int;
+}
+
+int HashTable::HashFunction1(int key) { return key % values_vector.size(); }
+
+int HashTable::HashFunction2(int key) { return 1 + key % (values_vector.size() - 1); }
+}  // namespace s21
+
+int main() {
+    s21::HashTable hash_table;
+    while (true) {
+        hash_table.Start();
+    }
 }
